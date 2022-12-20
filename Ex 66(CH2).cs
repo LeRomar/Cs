0e7b21599687a49794b91294068096dff4db8d2e
LeRomar <118387398+LeRomar@users.noreply.github.com>
2022-12-20 09:55:24 +0100

    Add files via upload

---
+++ Ex 66(CH2).cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+
+
+        public static void Main(string[] args)
+        {
+            int i, j, sqrt;
+            int[] T1 = { 4, 17, 5, 15 };
+
+
+            for (i = 0; i < 4; i++)
+            {
+              
+                   if (T1[i]
+ % 2 == 0)
+                {
+                    T1[i] = 2;
+                }
+
+                          
+    else
+                {
+                    if (T1[i] <= 3)
+                    {
+                        T1[i] = 1;
+                    }
+                    else
+                    {
+                        sqrt = (int)Math.Sqrt(T1[i]);
+                        for (j=2; j<=sqrt; j++)
+                        {
+                            if (T1[i] % j != 0)
+                            {
+                                T1[i] = 1;
+                            }
+                        }
+                    }
+
+                }
+            }
+            for (i = 0; i < 4; i++)
+            {
+                Console.WriteLine(T1[i]);
+            }
+        }
+    }
+}
---
+++ Ex 69(CH2).cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i;
+            double a, b;
+
+
+
+            double [] Tab  = { 8.2, 5.1, 12.9, -77.9, 6.4, -3.2, 19.8 };
+
+            for (i=0; i<3; i++)
+            {
+                a = Tab[i];
+                b = Tab[6 - i];
+                Tab[6 - i] = a;
+                Tab[i] = b;
+            
+            }
+
+            for (i=0; i<7; i++)
+            {
+                Console.WriteLine(Tab[i]);
+
+            }
+        }
+    }
+}
