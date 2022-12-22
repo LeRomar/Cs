b335327c0d321a6cc786bd7044e6126f2ff7eea9
Michael El Omar <quisutdeus100@gmail.com>
2022-12-22 21:55:30 +0100

    Fixing Exercise 66

--- Ex 66(CH2).cs
+++ Ex 66(CH2).cs
@@ -1,51 +1,45 @@
-﻿using System;
+using System;
 
 namespace HelloWorld
 {
     class MainClass
     {
-
-
         public static void Main(string[] args)
         {
-            int i, j, sqrt;
-            int[] T1 = { 4, 17, 5, 15 };
+            int i;
+            int[] T1 = {4, 17, 5, 15, 23, 47, 19};
 
-
-            for (i = 0; i < 4; i++)
+            // use T1.Length to get the length of the array
+            // instead of using 4. That way you can use the same code
+            // for any array of any length
+            for (i = 0; i < T1.Length; i++)
             {
-              
-                   if (T1[i]
- % 2 == 0)
-                {
+                /* if there is only one state inside the if block
+                 * then you can write without the curly braces {}
+                 */
+                if (T1[i] % 2 == 0)
                     T1[i] = 2;
-                }
-
-                          
-    else
+                else
                 {
-                    if (T1[i] <= 3)
-                    {
+                    /*
+                     * if there's a divider of T1[i] between 3 and Sqrt(T1[i])
+                     * then T1[i] is not a prime
+                     * so for all integers n starting from 3 
+                     * if n*n > T1[i] or T1[i] is divisible by n, then T1[i] is
+                     * not a prime.
+                     */
+                    int n = 3;
+                    while (n * n <= T1[i] && T1[i] % n != 0)
+                        n += 1;
+                    if (n * n > T1[i])
                         T1[i] = 1;
-                    }
-                    else
-                    {
-                        sqrt = (int)Math.Sqrt(T1[i]);
-                        for (j=2; j<=sqrt; j++)
-                        {
-                            if (T1[i] % j != 0)
-                            {
-                                T1[i] = 1;
-                            }
-                        }
-                    }
-
                 }
             }
-            for (i = 0; i < 4; i++)
-            {
+            /* Same with if block, with only one statement,
+             * no need for the curly braces {}
+             */
+            for (i = 0; i < T1.Length; i++)
                 Console.WriteLine(T1[i]);
-            }
         }
     }
 }
