9e604fd13b6d80460de065e8e34ffb701522867b
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-01-19 14:23:18 +0100

    Add files via upload

---
+++ Ex33 (CH4).cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //This algorithm shows the two highest numbers from the 6 numbers you enter 
+            int i, n, a, b, N;
+
+            N = 6;
+            Console.WriteLine("Entres les {0} valeurs: ",N);
+            a = int.Parse(Console.ReadLine());
+            b = a;
+
+            for (i=1; i<N; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+
+                if (n > b)
+                {
+                    a = b;
+                    b = n;
+                }
+                else
+                    if (n > a)
+                {
+                    a = n;
+                }
+                
+            }
+            Console.WriteLine("Les deux plus grandes valeurs: {0},{1} ", a, b);
+        }   
+    }
+}
