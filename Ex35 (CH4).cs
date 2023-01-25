95e0cf35100382763e92adfc54143e8eaa2bc693
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-01-25 21:51:07 +0100

    Add files via upload

---
+++ Ex35 (CH4).cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Inverse number with zeros on the left
+
+            int n;
+            Console.Write("Enter the number: ");
+            n = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write(n % 10);
+                n /= 10;
+
+            } while (n != 0);
+        }   
+    }
+}
