fcf881d0875a1b1ee122476112732c335de7e9ef
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-01-27 19:43:12 +0100

    Add files via upload

---
+++ Ex44 (CH4).cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n, i;
+            string F, A;
+            F = "-";
+            A = "*";
+
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                for(i=0; i <= n; i++)
+                {
+                    Console.Write(F);
+                }
+                Console.Write(A);
+                n--;
+            }
+
+        }
+    }
+}
