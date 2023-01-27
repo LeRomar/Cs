bd3d98cb0a8cb1f7ee3182e888381dd989fb8bcb
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-01-27 19:16:37 +0100

    Add files via upload

---
+++ Ex 39(CH4).cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //This algorithm calculates the average of the odd numbers
+            int n, i, x;
+            double M;
+            x = 0;
+            M = 0;
+
+            Console.WriteLine("Enter the numbers: ");
+            for(i=0; i<6; i++) 
+            {
+                n = int.Parse(Console.ReadLine());
+                if ((n % 2) != 0)
+                {
+                    M += n;
+                    x++;
+                }
+            }
+            Console.Write("La moyenne des nombres impaires est {0}", M / x);
+        }   
+    }
+}
