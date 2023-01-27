84e1b1108c4b21302d62e432c0a0554a3905f50b
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-01-27 19:21:14 +0100

    Add files via upload

---
+++ Ex43 (CH4).cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Calculates the sum of the digits
+            int n, S;
+
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            S = 0;
+
+            while (n != 0)
+            {
+                S += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine("La somme est {0} ", S);
+        }
+    }
+}
