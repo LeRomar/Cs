346953ca347a1a80dc3bd1a000586b8ac10a2a79
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-02-06 20:06:48 +0100

    Add files via upload

---
+++ Ex45 (CH4).cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character: ");
+            String N = Console.ReadLine();
+            Console.WriteLine("The height(odd number): ");
+            int H = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < H; i++)
+            {
+                for (int j = 0; j < H; j++)
+                {
+                    if (i == j || i + j == H-1)
+                    {
+                        Console.Write(N + " ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+
+        }
+
+
+    }
+}
