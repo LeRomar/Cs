b7b26cec7e33ea45b3cd91a25bcd5989126b7cad
LeRomar <118387398+LeRomar@users.noreply.github.com>
2023-03-12 14:17:40 +0100

    Add files via upload

---
+++ Cryptage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string Alphabet = "abcdefghijklmmnopqrstuvwxyz";
+            char[] chArray = Alphabet.ToCharArray();
+            string Phrase;
+            int i, j;
+
+            Phrase = Console.ReadLine();
+            Phrase.ToLower();
+            char[] phArray = Phrase.ToCharArray();
+            for (i = 0; i < Phrase.Length; i++)
+            {
+                for (j=0; j < Alphabet.Length; j++)
+                {
+                    if (phArray[i] == chArray[j] )
+                    {
+                        if (j <= 12) 
+                        {
+                            phArray[i] = chArray[j + 13];
+                        }
+                        if(j>12) 
+                        {
+                            phArray[i] = chArray[(j + 13) / 25];
+                        }
+
+                    }
+                }
+
+                Console.Write(phArray[i]);           
+            }
+
+
+
+
+
+        }
+    }
+} 
